f3c22aa4165e4010669a60faf3d0e99518934ba0
Mark <markhenningerwork@gmail.com>
2018-02-19 12:44:19 +0100

    add all routing for ideas, minions, meetings

--- server/api.js
+++ server/api.js
@@ -28,19 +28,10 @@ apiRouter.use((req, res, next) => {
 // ------ Routing Minions -----
 
 
-/*
-GET /api/minions to get an array of all minions.
-POST /api/minions to create a new minion and save it to the database.
-GET /api/minions/:minionId to get a single minion by id.
-PUT /api/minions/:minionId to update a single minion by id.
-DELETE /api/minions/:minionId to delete a single minion by id.
-*/
-
 apiRouter.get('/minions', (req, res, next) => {
   const allMinions = Helper.getAllFromDatabase('minions');
   res.send(allMinions);
 });
-
 
 apiRouter.get('/minions/:minionId', (req, res, next) => {
 
@@ -51,7 +42,6 @@ apiRouter.get('/minions/:minionId', (req, res, next) => {
   } else {
     res.send(minionById);
   }
-
 });
 
 apiRouter.put('/minions/:minionId', (req, res, next) => {
@@ -62,23 +52,17 @@ apiRouter.put('/minions/:minionId', (req, res, next) => {
     res.status(404).send('Minion does not exist');
   } else {
     const minionUpdate = Helper.updateInstanceInDatabase('minions', req.body);
-
     res.send(minionUpdate);
   }
-
-
 });
 
-
-
 apiRouter.post('/minions', (req, res, next) => {
-
     const newMinion = Helper.addToDatabase('minions', req.body);
     res.status(201).send(newMinion);
 });
 
 apiRouter.delete('/minions/:minionId', (req, res, next) => {
-  
+
   const minionById = Helper.getFromDatabaseById('minions', req.params.minionId);
 
   if (minionById === -1 || minionById === undefined) {
@@ -87,6 +71,87 @@ apiRouter.delete('/minions/:minionId', (req, res, next) => {
     const minionDelete = Helper.deleteFromDatabasebyId('minions', req.params.minionId);
     res.status(204).send('No content');
   }
+});
 
+// ------ Routing Ideas -----
+
+/*
+/api/ideas
+GET /api/ideas to get an array of all ideas.
+POST /api/ideas to create a new idea and save it to the database.
+GET /api/ideas/:ideaId to get a single idea by id.
+PUT /api/ideas/:ideaId to update a single idea by id.
+DELETE /api/ideas/:ideaId to delete a single idea by id.
+*/
+
+apiRouter.get('/ideas', (req, res, next) => {
+  const allMinions = Helper.getAllFromDatabase('ideas');
+  res.send(allMinions);
+});
+
+apiRouter.get('/ideas/:ideaId', (req, res, next) => {
+
+  const minionById = Helper.getFromDatabaseById('ideas', req.params.ideaId);
+
+  if (minionById === -1 || minionById === undefined) {
+    res.status(404).send('Idea does not exist');
+  } else {
+    res.send(minionById);
+  }
+});
+
+apiRouter.put('/ideas/:ideaId', (req, res, next) => {
+
+  const minionById = Helper.getFromDatabaseById('ideas', req.params.ideaId);
+
+  if (minionById === -1 || minionById === undefined) {
+    res.status(404).send('Idea does not exist');
+  } else {
+    const minionUpdate = Helper.updateInstanceInDatabase('ideas', req.body);
+    res.send(minionUpdate);
+  }
+});
+
+apiRouter.post('/ideas', (req, res, next) => {
+    const newMinion = Helper.addToDatabase('ideas', req.body);
+    res.status(201).send(newMinion);
+});
+
+apiRouter.delete('/ideas/:ideaId', (req, res, next) => {
+
+  const minionById = Helper.getFromDatabaseById('ideas', req.params.ideaId);
+
+  if (minionById === -1 || minionById === undefined) {
+    res.status(404).send('Idea does not exist');
+  } else {
+    const minionDelete = Helper.deleteFromDatabasebyId('ideas', req.params.ideaId);
+    res.status(204).send('No content');
+  }
+});
+
+// ------ Routing Meetings -----
+
+/*
+/api/meetings
+GET /api/meetings to get an array of all meetings.
+POST /api/meetings to create a new meeting and save it to the database.
+DELETE /api/meetings to delete all meetings from the database.
+*/
+
+apiRouter.get('/meetings', (req, res, next) => {
+  const allmeetings = Helper.getAllFromDatabase('meetings');
+  res.send(allmeetings);
+});
+
+apiRouter.post('/meetings', (req, res, next) => {
+    const newMeeting = Helper.addToDatabase('meetings', Helper.createMeeting());
+    res.status(201).send(newMeeting);
+});
+
+apiRouter.delete('/meetings', (req, res, next) => {
+
+  const deletedMeetings = Helper.deleteAllFromDatabase('meetings');
+
+  res.status(204).send();
 
 });
